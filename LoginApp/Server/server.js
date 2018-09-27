2216826b3fd7ae2ce729c5d19c94f0ddc030d0c2
Felipe Valencia <>
2018-09-27 17:49:57 -0500

    Started Client App

--- LoginApp/Server/server.js
+++ LoginApp/Server/server.js
@@ -1,7 +1,10 @@
 
+const MySQL = require("MySQL");
+const nodemailer = require("nodemailer");
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const bodyparser = require("body-parser");
 const CryptoJS = require("crypto-js");
 const con = mysql.createConnection({
     host: "localhost",
@@ -9,34 +12,86 @@ const con = mysql.createConnection({
     password: "castro03",
     database: "LoginDB"
 });
-app.use(function(req, res, next) {
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'redpandamc85@gmail.com',
+        pass: 'helpingoTES'
+    }
+});
+app.use(express.static("../public"));
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-/* Encryption:
-var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123');
-    var bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
-    var plaintext = bytes.toString(CryptoJS.enc.Utf8);
- */
-function decrypt(encrypted,userkey) {
-    let bytes = CryptoJS.AES.decrypt(encrypted.toString(),userkey.toString());
+let urlencodedparser = bodyparser.urlencoded({extended: false});
+function decrypt(encrypted, userkey) {
+    let bytes = CryptoJS.AES.decrypt(encrypted.toString(), userkey.toString());
     return bytes.toString(CryptoJS.enc.Utf8);
 }
+
 function encrypt(plaintext, userkey) {
     return CryptoJS.AES.encrypt(plaintext, userkey)
 }
-app.get("/",(req,res) => {
-res.redirect("/login");
-res.status(200);
+
+app.get("/", (req, res) => {
+    res.redirect("/index");
+    res.status(200);
 });
-app.get("/login", (req,res) => {
+app.post("/login", urlencodedparser, (req, res) => {
+    let username = req.body.user;
+    let pass = req.body.pass;
+        let query = "SELECT Password FROM User WHERE Username = " + mysql.escape(username);
+        con.query(query, function (err, result) {
+            if (err) throw err;
+            let password = result[0].Password;
+            let plainpass = decrypt(password,username);
+            let bool;
+            if (plainpass === pass) {
+                 bool = true
+            }else {
+                 bool = false
+            }
+            res.send(bool)
+        });
 
 });
-app.post("/signup",(req,res) => {
+app.post("/signup",urlencodedparser, (req, res) => {
+    let username = req.body.user;
+    let pass = req.body.pass;
+    let email = req.body.email;
+    const link = ("http://192.168.0.6:8081/new?user="+username+"&&pass="+pass+"&&email="+email).toString();
+    const mailOptions = {
+        from: 'redpandamc85@gmail.com',
+        to: email,
+        subject: 'Sign Up Verification Code:',
+        html: "<h1>Verify your account here:</h1><br><a style='font-size: 20px;' href="+link+">Verify Here</a>"
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+            res.status(500).send();
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send(true)
+        }
+    });
 
 });
-app.get("/new", (req,res) => {});
+app.get("/new", (req, res) => {
+    const user = req.query.user;
+    const pass = req.query.pass;
+    let email = req.query.email;
+    email = email.toString();
+    let cipherpass = encrypt(pass,user);
+    const sql = "INSERT INTO User (Username, Email, Password) VALUES ('"+user+"', '"+email+"','"+cipherpass+"')";
+    con.query(sql, (err,result) => {
+        if (err) throw err;
+        res.status(200).send(true);
+    })
+
+});
 const server = app.listen(8081, function () {
     const host = server.address().address;
     const port = server.address().port;
