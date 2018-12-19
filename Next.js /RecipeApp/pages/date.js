4f6e635139b85ba0a8ab76e5ef238df5c9bc503c
Felipe Valencia <>
2018-12-18 20:25:13 -0500

    started Gonzas App

--- Next.js /RecipeApp/pages/date.js
+++ Next.js /RecipeApp/pages/date.js
@@ -6,6 +6,9 @@ export default class Date extends React.Component {
     componentDidMount() {
         this.interval = setInterval(() => {this.setState({secs: new Date().getTime() /1000})},1000)
     }
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
     render() {
         return(
             <div>
